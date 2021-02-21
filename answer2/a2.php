b5b71d54063c3fabb497ce09709ecf834fac181d
浜中望帆 <miho-hamanaka@se-ec.co.jp>
2021-02-22 08:21:20 +0900

    問1,2,3,7を回答

--- answer2/a2.php
+++ answer2/a2.php
@@ -4,21 +4,54 @@
     require_once '../common/layout.php';
     echo $head;
     require_once '../common/question.php';
+
     //▼ここから答えのプログラム▼
+    function calculation($num1,$num2) {
+        $applePrice = 150;
+        $orangePrice = 80;
+        return $applePrice * $num1 + $orangePrice * $num2.'円';
+    }
+    
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && 
+    isset($_POST['apple'])&& 
+    isset($_POST['orange'])) {//入力値セットされた時
 
+        $appleNum = mb_convert_kana($_REQUEST['apple'], 'n','UTF-8');//全角は半角に
+        //$appleNum = $_POST['apple'];
+        $orangeNum = mb_convert_kana($_REQUEST['orange'], 'n','UTF-8');
+        //$orangeNum = $_POST['orange'];
+        
+        if(is_numeric($appleNum) && is_numeric($orangeNum)) {//半角数字の時だけ
+            //$total = $applePrice * $appleNum + $orangePrice * $orangeNum.'円';
+            $total = calculation($appleNum,$orangeNum);
+        }
+        else {
+            $total = '数値が未入力です';//半角数字ではない時
+        }
 
+    } else {
+        $total = '0円';//初期値
+    }
 ?>
 
 
 <body>
     <div id="layout">
-    <p><?php echo $title; ?></p>
-    <?php echo $question2; ?>    
-    <!-- ▼ここから答えの記述▼ -->
+        <p><?php echo $title; ?></p>
+        <?php echo $question2; ?>
 
-    <p>未回答</p>
-
+        <!-- ▼ここから答えの記述▼ -->
+        <p>りんごは1つ150円、みかんは1つ80円です。個数を入力してください<br>
+        ※半角数字で入力してください。</p>
+        <form action="" method="post">
+            りんご<input type="text" name="apple" style="width:40px;">個
+            みかん<input type="text" name="orange" style="width:40px;">個　
+            <input type="submit" value="計算">        
+        </form>
+        <div style="text-decoration:underline;">
+        <br>
+        <p>合計: <?php echo $total; ?></p>
+        </div>
     </div>
-
 </body>
 </html>
--- answer7/a7.php
+++ answer7/a7.php
@@ -4,8 +4,33 @@
     require_once '../common/layout.php';
     echo $head;
     require_once '../common/question.php';
+    
     //▼ここから答えのプログラム▼
 
+    date_default_timezone_set("Asia/Tokyo");
+    $timestamp = time();
+
+    $dataFile = 'test.txt';
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && 
+    isset($_POST['message'])) {
+
+        $message = trim($_POST['message']);
+
+        if ($message !== ''){
+            $message = str_replace("\t", '', $message); //タブが入力されたら空白にする
+            $postedAt = date('Y-m-d');
+            $newData = $message . "\t" . $postedAt . "\n";
+
+            $fp = fopen($dataFile, 'a');
+            fwrite($fp, $newData);
+            fclose($fp);
+        }
+    }
+
+    // 投稿の表示
+    $posts = file($dataFile, FILE_IGNORE_NEW_LINES);
+    $posts = array_reverse($posts);
 
 ?>
 
@@ -16,9 +41,25 @@
     <?php echo $question7; ?>    
     <!-- ▼ここから答えの記述▼ -->
 
-    <p>未回答</p>
+    <h1>簡易掲示板</h1>
+    <form action="" method="post">
+        投稿文: <input type="text" name="message">
+        <input type="submit" value="投稿">
+    </form>
+
+    <!-- 入力文字を表示できるようにしています -->
+    <h2>投稿一覧(<?php echo count($posts) ?>件)</h2>
+        <ul>
+            <?php if (count($posts)) :?>
+                <?php foreach ($posts as $post) :?>
+                <?php list($message, $postedAt) = explode("\t", $post); ?>
+                    <li><?php echo htmlspecialchars($message)?> - <?php echo htmlspecialchars($postedAt); ?></li>
+                <?php endforeach; ?>
+            <?php else :?>
+                <li>まだ投稿はありません</li>
+            <?php endif; ?>
+        </ul>
 
     </div>
-
 </body>
 </html>
